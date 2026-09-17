9b7c49470fe789896705d2b463c5e6020397364b
zrubichon <zoe.rubichon@gmail.com>
2026-09-17 07:17:30 +0200

    Make authenticated account page dynamic

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { signOut } from "./actions";
 
+export const dynamic = "force-dynamic";
+
 export default async function AccountPage() {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
